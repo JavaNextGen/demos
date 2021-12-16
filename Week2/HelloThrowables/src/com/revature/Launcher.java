3bf59ebf09dcbc705c4bd5bd362196eb02fca055
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-12-16 15:38:39 -0500

    finished HelloThrowables

--- Week2/HelloThrowables/src/com/revature/Launcher.java
+++ Week2/HelloThrowables/src/com/revature/Launcher.java
@@ -3,6 +3,8 @@
  */
 package com.revature;
 
+import java.util.Scanner;
+
 import com.revature.exceptions.MyCheckedException;
 import com.revature.exceptions.MyUncheckedException;
 
@@ -12,13 +14,75 @@ import com.revature.exceptions.MyUncheckedException;
  */
 public class Launcher {
 	
-	public static void main(String[] args) {
+	public static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) throws MyCheckedException {
+		
+		//throwUnchecked(); //the compiler will let you this method... but it'll just end up crashing your program
+		//the compiler is not checking for unchecked exceptions... don't need a throws declaration
+		
+		//throwChecked(); //again, you can run this if there is a "throws MyCheckedException" declaration in main
+		//but again it will crash your program.
+		
+		System.out.println("=====================================(Try/Catch)");
+		
+		try { //try will TRY a block of code that may or may not throw an exception
+			
+			System.out.println("try block starting...");
+			
+			throwChecked();
+			
+			System.out.println("I won't print - once the Exception is thrown, the rest of the try block is skipped");
+			
+		} catch (MyUncheckedException e) {
+			
+			System.out.println("Will I run? No!!!!! I'm only checking for MyUncheckedException");
+			
+		} catch (MyCheckedException e) {
+			
+			System.out.println("Hello from the catch block - MyCheckedException was caught!");
+			
+		} catch (Exception e) {
+			
+			System.out.println("I could've caught any kind of Exception");
+			
+		} finally {
+			System.out.println("Hello from the finally block - I will always run");
+		}
 		
 		
+		System.out.println("The program is still running, thanks to our try/catch");
+		
+		//It's best practice to write your catch blocks from most specific to least specific
+		//Very common to see the final catch block catch just a generic Exception
+		//Also, if you put the most generic exception up top, you'll get "unreachable code" errors
+		
+		
+		//trying to catch an ArithmeticException (which we will never cause on purpose but we will here for science)
+
+		
+		try {
+			
+			System.out.println("I'm going to try to divide by zero >:)");
+			
+			System.out.println("What is your age?");
+			int age = scan.nextInt();
+			scan.nextLine();
+			
+			System.out.println("How many years have you been coding?");
+			int codingExp = scan.nextInt();
+			scan.nextLine();
+			
+			System.out.println("You have been alive "+(age/codingExp)+" times more than you have been coding");		
+			
+			//this is unrealistic btw, we'd never do this on purpose		
+			
+		} catch (ArithmeticException e){
+			System.out.println("You haven't been coding long.");
+		}
 		
 	}
-	
-	
+
 	
 	//creating methods that throw our custom exceptions---------
 	
