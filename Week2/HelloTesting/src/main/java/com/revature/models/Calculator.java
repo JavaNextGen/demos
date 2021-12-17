1054f629cec7b2e6d448b939ae10dfeb856edb62
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2021-12-17 11:53:52 -0500

    added calculator class

---
+++ Week2/HelloTesting/src/main/java/com/revature/models/Calculator.java
@@ -0,0 +1,23 @@
+package com.revature.models;
+
+public class Calculator {
+
+	//we're going to make 3 super simple methods to write unit tests for
+	//remember, unit tests tend to test the smallest possible blocks of code (usually methods)
+	
+	//addition method
+	public int add(int x, int y) {
+		return x + y;
+	}
+	
+	//subtraction method
+	public int subtract(int x, int y) {
+		return x - y;
+	}
+	
+	//division method
+	public int divide(int x, int y) {
+		return x / y;
+	}
+	
+}
