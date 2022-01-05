c45fdc6eb2209746eb05e723b6a42bcfa309cba1
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-01-05 16:48:05 -0500

    HelloFetch

--- Week5/javascript/demos/Ben_Demos/HelloFetch/HelloFetch.js
+++ Week5/javascript/demos/Ben_Demos/HelloFetch/HelloFetch.js
@@ -1,48 +1,48 @@
+//PokeAPI URL - making a variable to hold the URL, which will make for cleaner code
+const url = "https://pokeapi.co/api/v2/pokemon/";
 
-const api="https://pokeapi.co/api/v2/pokemon/";
-
-
+//when the user clicks the submit button, execute the getData function
 document.getElementById("btn").onclick = getData;
 
+//This function will return our pokemon data, just like how we did in HelloAjax
+//BUT I'm going to use the fetch api to return a promise object
+//Remember, async tells a function to return a promise object instead of a direct value
+async function getData(){
 
-
-
-async function getData() {
-
-    
+    //put the user input into a variable
     let userInput = document.getElementById("userInput").value;
 
-    
-    let response = await fetch(api+userInput); 
-    
+    //use the fetch() method to send a request to the PokeAPI with the user's input
+    let response = await fetch(url + userInput); //remember, this is a PROMISE object
+    //remember, await pauses the function until the promise is returned.
 
-    if(response.status === 200){ 
-        let data = await response.json(); 
-        populateData(data); 
+    if(response.status === 200){ //if the response came back with a successsful status code 
+        let data = await response.json(); //parse the JSON in the response and turn it into a JS object
+        renderHTML(data);
     } else {
         document.getElementById("data").innerHTML = "It got away!";
     }
 }
+//compare the getData() function to the ajaxFunc() in HelloAjax. This is cleaner/more concise
 
+//this function will use the JS object we parsed from the response to populate our webpage with data
+function renderHTML(data) {
 
+    let dataSection = document.getElementById("data"); //this is our currently empty section element
 
-
-function populateData(data) {
-
-    let dataSection = document.getElementById("data"); 
-
-    let nameTag = document.createElement("h3"); 
-    nameTag.innerHTML = data.name; 
+    let nameTag = document.createElement("h3"); //create a header for the pokemon's name
+    nameTag.innerHTML = data.name; //assign the pokemon's name to the new header
 
     let pokePic = document.createElement("img");
     pokePic.setAttribute("src", data.sprites.front_default);
 
     let pokePic2 = document.createElement("img");
-    pokePic2.setAttribute("src", data.sprites.back_default);
+    pokePic2.setAttribute("src", data.sprites.front_shiny);
 
-
+    //actually populate the section element with the data gathered from the response
     dataSection.appendChild(nameTag);
     dataSection.appendChild(pokePic);
     dataSection.appendChild(pokePic2);
-
 }
+
+
