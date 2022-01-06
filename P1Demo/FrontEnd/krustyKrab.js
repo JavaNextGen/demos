4c632d88f1a64cf96cd84b1facb3ca3759aeb6cc
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-01-06 12:44:02 -0500

    render employee data in P1Demo

--- P1Demo/FrontEnd/krustyKrab.js
+++ P1Demo/FrontEnd/krustyKrab.js
@@ -9,6 +9,7 @@ document.getElementById("getEmployeeButton").addEventListener("click", getEmploy
 async function getEmployees() {
 
     //we will send a fetch request to get our employee data
+    //by default, fetch requests send GET requests
     let response = await fetch(url + "employee");
 
     //logging the response in the console just to see the response object
@@ -36,6 +37,18 @@ async function getEmployees() {
             row.appendChild(cell);
 
             //we'll do this^ for every column in employees
+
+            let cell2 = document.createElement("td");
+            cell2.innerHTML = employee.f_name;
+            row.appendChild(cell2);
+
+            let cell3 = document.createElement("td");
+            cell3.innerHTML = employee.l_name;
+            row.appendChild(cell3);
+
+            let cell4 = document.createElement("td");
+            cell4.innerHTML = employee.role_id;
+            row.appendChild(cell4);
 
             //append the tr called row that we created in the for loop to the table body
             //a new row will be appended for every employee object that comes in
