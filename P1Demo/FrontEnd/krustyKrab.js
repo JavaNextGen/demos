62a504f457e9c54db586ff95a8dbdbac2fdbd825
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-01-06 11:20:27 -0500

    little little javascript in P1Demo

---
+++ P1Demo/FrontEnd/krustyKrab.js
@@ -0,0 +1,6 @@
+const url = "http://localhost:3000/" //putting our base URL in a variable for cleaner code below
+//eventually, we'll use this in our fetch requests and make calls to our server by appending endpoints
+
+//add eventListeners to our buttons
+
+
