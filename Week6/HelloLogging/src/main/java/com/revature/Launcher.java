65755c795324c7e05dc1303ac756a0f27e60ee5b
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-01-18 15:59:13 -0500

    configured HelloLogging

--- Week6/HelloLogging/src/main/java/com/revature/Launcher.java
+++ Week6/HelloLogging/src/main/java/com/revature/Launcher.java
@@ -2,4 +2,14 @@ package com.revature;
 
 public class Launcher {
 
+	public static void main(String[] args) {
+		
+		//Instantiate a Logger object
+		
+		//Write some code to log 
+		
+		
+		
+	}
+	
 }
