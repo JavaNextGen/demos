3178e17d8fb2a5ede6b254194bcc284b776af4e9
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-01-24 13:00:43 -0500

    applicationContext (way 1 of 3)

--- Week7/HelloSpring/src/main/java/com/revature/models/User.java
+++ Week7/HelloSpring/src/main/java/com/revature/models/User.java
@@ -6,6 +6,100 @@ import org.springframework.stereotype.Component;
 //Ben will mess with scoping here... Remember the default bean scope is Singleton
 public class User {
 
+	private int id;
+	private String name;
+	
+	private Account account;
+
+	
+	//boilerplate code--------------------------------------------
+	
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public User(int id, String name, Account account) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.account = account;
+	}
+
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", account=" + account + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((account == null) ? 0 : account.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (account == null) {
+			if (other.account != null)
+				return false;
+		} else if (!account.equals(other.account))
+			return false;
+		if (id != other.id)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Account getAccount() {
+		return account;
+	}
+
+
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+	
 	
 	
 }
