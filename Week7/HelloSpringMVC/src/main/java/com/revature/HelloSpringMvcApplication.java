8b6aa972c62dfceb49a7c0ed314cff778dff8148
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-01-26 11:54:36 -0500

    HelloSpringAOP pom.xml

--- Week7/HelloSpringMVC/src/main/java/com/revature/HelloSpringMvcApplication.java
+++ Week7/HelloSpringMVC/src/main/java/com/revature/HelloSpringMvcApplication.java
@@ -8,6 +8,11 @@ public class HelloSpringMvcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloSpringMvcApplication.class, args);
+		
+		java.sql.Date date = java.sql.Date.valueOf("2022-05-01");
+		
+		System.out.println(date);
+		
 	}
 
 }
