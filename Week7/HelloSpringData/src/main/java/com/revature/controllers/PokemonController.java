52c7974fcbbdbf03da350ab06c8548d0bc791377
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-01-27 12:15:38 -0500

    hehe

---
+++ Week7/HelloSpringData/src/main/java/com/revature/controllers/PokemonController.java
@@ -0,0 +1,45 @@
+package com.revature.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.models.Pokemon;
+import com.revature.repositories.PokemonDAO;
+
+@RestController //This annotation combines @Controller and @ResponseBody. 
+	//So this class is now a Bean that can use MVC annotations and sends responses back in JSON format\
+@RequestMapping(value="/pokemon") //any request ending in /pokemon will go to this controller
+public class PokemonController {
+
+	//I just need to autowire the DAO class to this class
+	//Like in the last MVC demo, I'm skipping the service layer just for time, but you should still use services
+	private PokemonDAO pDAO;
+	
+	//so now, PokemonController will automagically have a PokemonDAO associated with it
+	@Autowired
+	public PokemonController(PokemonDAO pDAO) {
+		this.pDAO = pDAO;
+	}
+	
+	
+	//MVC methods below (to handle HTTP requests)------------------------------
+	
+	//These SHOULD probably call a service layer that's calling the DAO
+		//so .body() would contain a call to the service method that gets the appropriate data from the DAO
+
+
+
+
+	@PostMapping //every POST request ending in /pokemon goes here
+	public ResponseEntity addPokemon(@RequestBody Pokemon p) {
+		pDAO.save(p); //the save() method is how we do INSERTS
+		return ResponseEntity.status(201).build();
+		//we use .build() instead of .body() if we don't intend to send data back.
+	}
+	
+	
+}
--- Week7/HelloSpringData/src/main/java/com/revature/repositories/PokemonDAO.java
+++ Week7/HelloSpringData/src/main/java/com/revature/repositories/PokemonDAO.java
@@ -1,12 +1,17 @@
 package com.revature.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.revature.models.Pokemon;
 
-//Ben will explain this LEAVING COMMENT HERE TO REMEBER TO COMMENT AFTER BREAK
+//By extending JpaRepository, we get access to various DAO methods that we don't have to write ourselves
+//JpaRepository takes two generics... (Pokemon and Integer in our case)
+	//The first generic is the entity being persisted
+	//The second generic is the data type of the primary key (as a wrapper class)
+@Repository
 public interface PokemonDAO extends JpaRepository<Pokemon, Integer> {
 
-
+	
 	
 }
