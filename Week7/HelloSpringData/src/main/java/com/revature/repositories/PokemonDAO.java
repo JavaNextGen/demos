ccb0adbb4b4d49d517957399849040dfd6fa07d3
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-01-27 11:26:12 -0500

    HelloSpringData Pokemon Class

--- Week7/HelloSpringData/src/main/java/com/revature/repositories/PokemonDAO.java
+++ Week7/HelloSpringData/src/main/java/com/revature/repositories/PokemonDAO.java
@@ -2,6 +2,11 @@ package com.revature.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PokemonDAO extends JpaRepository {
+import com.revature.models.Pokemon;
 
+//Ben will explain this LEAVING COMMENT HERE TO REMEBER TO COMMENT AFTER BREAK
+public interface PokemonDAO extends JpaRepository<Pokemon, Integer> {
+
+
+	
 }
