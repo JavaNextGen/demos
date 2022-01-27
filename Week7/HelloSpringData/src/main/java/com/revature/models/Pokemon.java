170145dfdc1f201742f7022f2cd1cc3206b909b1
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-01-27 11:23:56 -0500

    HelloSpringData Pokemon Class

--- Week7/HelloSpringData/src/main/java/com/revature/models/Pokemon.java
+++ Week7/HelloSpringData/src/main/java/com/revature/models/Pokemon.java
@@ -1,5 +1,135 @@
 package com.revature.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//We're using Spring Data JPA, but basic model classes should look just like Hibernate
+
+@Entity
+@Table(name="pokemon")
 public class Pokemon {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	private String type;
+	private int level;
+
+	
+	//boilerplate code------------------------------------------------------------
+	
+	public Pokemon() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Pokemon(int id, String name, String type, int level) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.type = type;
+		this.level = level;
+	}
+
+	//constructor with no ID, so we can persist new pokemon to the database
+	public Pokemon(String name, String type, int level) {
+		super();
+		this.name = name;
+		this.type = type;
+		this.level = level;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getType() {
+		return type;
+	}
+
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+
+	public int getLevel() {
+		return level;
+	}
+
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Pokemon [id=" + id + ", name=" + name + ", type=" + type + ", level=" + level + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		result = prime * result + level;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pokemon other = (Pokemon) obj;
+		if (id != other.id)
+			return false;
+		if (level != other.level)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (type == null) {
+			if (other.type != null)
+				return false;
+		} else if (!type.equals(other.type))
+			return false;
+		return true;
+	}
+	
+	
+	
 }
---
+++ Week7/HelloSpringData/src/main/java/com/revature/repositories/PokemonDAO.java
@@ -0,0 +1,7 @@
+package com.revature.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PokemonDAO extends JpaRepository {
+
+}
