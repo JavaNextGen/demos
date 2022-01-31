d8cb07997618f2da478c173e0b46ad8712e27b09
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-01-31 12:27:41 -0500

    Date component in todoList

--- Week8/todo-list/src/app/components/todos-date/todos-date.component.ts
+++ Week8/todo-list/src/app/components/todos-date/todos-date.component.ts
@@ -1,3 +1,4 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosDateComponent implements OnInit {
 
-  myDate:Date = new Date();
+  //myDate:Date = new Date(); 
+  //This gave us way too much information
+
+  //We found the formatDate() function which returns a String with the date format you like
+  myDate:string = formatDate(new Date(), 'yyyy/MM/dd', 'en');
 
   constructor() { }
 
