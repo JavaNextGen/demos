0c1ed064e182f898576cab513f1d014985dca820
Benjamin Petruzziello <benjamin.petruzziello@revature.com>
2022-02-01 12:32:57 -0500

    can get pokemon :)

--- Week8/ngPokes/src/app/components/pokedex/pokedex.component.ts
+++ Week8/ngPokes/src/app/components/pokedex/pokedex.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Pokemon } from 'src/app/models/pokemon';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-pokedex',
@@ -10,15 +12,33 @@ export class PokedexComponent implements OnInit {
   //this is the input variable referred to by the two way binding in our HTML
   public input:number = 0;
 
-  public pokemon:any = "placeholder";
+  public pokemon:any = null;
 
-  getPoke():void{
-    console.log("hi");
-  }
-
-  constructor() { }
+  //constructor injection so that we can use the getPokemonFromApi function of the service
+  constructor(private ps:PokemonService) { }
 
   ngOnInit(): void {
+  }
+
+  //This is the function that gets our Pokemon object from the Pokemon Service
+  //the Service has a function that gets a Pokemon as an observable
+  //In order to get data from an Observable we need to SUBSCRIBE to it
+  getPoke():void{
+    this.ps.getPokemonFromApi(this.input).subscribe(
+
+      //get the data out of the observable that we subscribed to, and put it into a Pokemon object
+      (data:any) => {
+        //assign it to our Pokemon variable above
+        this.pokemon = data;
+        //we may have to do something with sprites
+        console.log(this.pokemon) //will be helpful for debugs
+      },
+      () => { //in case of errors, set pokemon to null since we didn't get anything back
+        this.pokemon = null;
+        console.log("It got away!!")
+      }
+
+    )
   }
 
 }
